8240ab21a03f50800df1e445f9868ed54c94254d
marix05 <97682351+marix05@users.noreply.github.com>
2023-07-17 15:07:02 +0700

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -18,49 +18,9 @@ class AdminSeeder extends Seeder
         Admin::truncate();
 
         Admin::create([
-            'username' => 'inti',
-            'password' => bcrypt('sukses123'),
+            'username' => 'exm',
+            'password' => bcrypt('exm'),
             'session' => 'User',
-        ]);
-        Admin::create([
-            'username' => 'admin',
-            'password' => bcrypt('amin123'),
-            'session' => 'User',
-        ]);
-        Admin::create([
-            'username' => 'photography',
-            'password' => bcrypt('photo123'),
-            'session' => 'Photography',
-        ]);
-        Admin::create([
-            'username' => 'businessplan',
-            'password' => bcrypt('bpc123'),
-            'session' => 'BusinessPlan',
-        ]);
-        Admin::create([
-            'username' => 'uiuxdesign',
-            'password' => bcrypt('uiux123'),
-            'session' => 'UIUXDesign',
-        ]);
-        Admin::create([
-            'username' => 'valorant',
-            'password' => bcrypt('valo123'),
-            'session' => 'Valorant',
-        ]);
-        Admin::create([
-            'username' => 'mobilelegends',
-            'password' => bcrypt('mole123'),
-            'session' => 'MobileLegends',
-        ]);
-        Admin::create([
-            'username' => 'seminar',
-            'password' => bcrypt('seminar123'),
-            'session' => 'Seminar',
-        ]);
-        Admin::create([
-            'username' => 'marketing',
-            'password' => bcrypt('marketing123'),
-            'session' => 'Marketing',
         ]);
     }
 }
