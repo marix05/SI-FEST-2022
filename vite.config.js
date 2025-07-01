b9d674dcdfe513c6fba5d51ba6e966c135db6079
Marix Marchellino Susanto <marixsusantooo@gmail.com>
2025-07-01 15:38:10 +0700

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    base: "/build/", // 👈 penting untuk routing asset di Vercel
+    build: {
+        outDir: "public/build", // 👈 output ke folder public
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            input: ["resources/css/app.css", "resources/js/app.js"],
+        },
+    },
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
